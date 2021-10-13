5353b01be1beadf62804ae5b148d6bf61d4c6e2c
Dani <dani.cstech@gmail.com>
2021-10-13 21:14:13 +0800

    backend and frontend projects initialized

---
+++ ecrmwa-backend/src/test/java/com/example/ecrmwabackend/EcrmwaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecrmwabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcrmwaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
