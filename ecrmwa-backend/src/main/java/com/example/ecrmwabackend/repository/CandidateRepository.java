aea727a2ce1133540911510752b69cfe2544b630
apmbonifacio <60639961+apmbonifacio@users.noreply.github.com>
2021-10-18 14:52:56 +0800

    candidate-campaign integration
    integrated candidates with campaigns
    
    changed models from bi-directional to unidirectional relationships. now only campaigns "knows" its relationship with candidates and elecper.
    
    Also, deleting a candidate or elecper will also delete campaigns with fks to them.
    
    nakisabay: updated hashcode, equals, toString methods of campaigns and elecper

--- ecrmwa-backend/src/main/java/com/example/ecrmwabackend/repository/CandidateRepository.java
+++ ecrmwa-backend/src/main/java/com/example/ecrmwabackend/repository/CandidateRepository.java
@@ -1,14 +1,7 @@
 package com.example.ecrmwabackend.repository;
 
 import com.example.ecrmwabackend.model.Candidate;
-// <<<<<<< Campaign-Elecper
-// import org.springframework.data.repository.CrudRepository;
-// import org.springframework.stereotype.Repository;
 
-// @Repository
-// public interface CandidateRepository extends CrudRepository<Candidate, Long> {
-
-// =======
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
