781a5ff639441089d97c4d91c646a168e82fbc8c
Dani <dani.cstech@gmail.com>
2021-10-18 21:38:05 +0800

    elecper template hierarchy experiments

--- ecrmwa-frontend/src/app/elecper/elecper-routing.module.ts
+++ ecrmwa-frontend/src/app/elecper/elecper-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ElecperComponent } from './elecper.component';
+import { ElecperModule } from './elecper.module';
 import { ShowElecpersComponent } from './show-elecpers/show-elecpers.component';
 
 const routes: Routes = [
+  { path: '', component: ElecperComponent },
   { path: 'all', component: ShowElecpersComponent }
 ];
 
