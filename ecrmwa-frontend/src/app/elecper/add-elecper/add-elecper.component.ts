27fbb0fa77c88e9a1b0de389e9fbf0698a7acbba
Dani <dani.cstech@gmail.com>
2021-10-23 12:14:38 +0800

    add elections redirect working

--- ecrmwa-frontend/src/app/elecper/add-elecper/add-elecper.component.ts
+++ ecrmwa-frontend/src/app/elecper/add-elecper/add-elecper.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Elecperservice } from '../service/elecperservice';
 
 @Component({
@@ -11,19 +12,20 @@ import { Elecperservice } from '../service/elecperservice';
 export class AddElecperComponent implements OnInit {
   addElecperURL = "http://localhost:18080/ElecperTester/addElecper";
 
-  constructor(private elecperService: Elecperservice) { }
+  constructor(private elecperService: Elecperservice, private router: Router) { }
 
   ngOnInit(): void {
     
   }
 
   onSubmit(addElecper: NgForm) {
+    console.log(addElecper.value);
     addElecper.value["archived"] = false;
     this.elecperService.addElecper(addElecper.value).subscribe(
       (response) => console.log(response),
       (error) => console.log(error)
     );
-    
+    this.router.navigate(['/elections/all'])
   }
 
 }
